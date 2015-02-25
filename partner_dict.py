155c06f3e139a3eca589f95bd3e5d900440918d4
Addilah Zakaria <addilahz@addilahs-mbp.attlocal.net>
2015-02-24 19:37:49 -0800

    finishing partner dict and types exercise

--- partner_dict.py
+++ partner_dict.py
@@ -1,3 +1,38 @@
 cs_partner_info = {}
-cs_partner_info ["age"] = 19
-cs_partner_info ["eye color"] = 'brown'
+cs_partner_info ['age'] = 26
+cs_partner_info ['eye color'] = 'dark brown'
+cs_partner_info ['fave color'] = 'blue'
+cs_partner_info ['fave food'] = 'pesto pizza'
+cs_partner_info ['current time'] = '5:02pm'
+cs_partner_info ['last name'] = 'zakaria'
+cs_partner_info ['fave song'] = 'song1: thinking out loud, song2: shake it off, song3: sugar'
+cs_partner_info ['fave no'] =  '2, 12, 22, 82'
+
+
+#print current time
+print cs_partner_info ['current time']
+
+# print NOW as current time
+cs_partner_info ['current time'] = 'NOW'
+print cs_partner_info ['current time']
+
+#print entire dictionary
+print cs_partner_info
+
+#delete eye color & print again entire dictionary 
+del cs_partner_info ['eye color']
+print cs_partner_info
+
+#delete fave color
+del cs_partner_info ['fave color']
+print cs_partner_info
+
+#check last name in dictionary
+'last name' in cs_partner_info
+#answer: True becuase last name is in the dictionary
+
+# check age
+'age' in cs_partner_info
+#answer: True because theres an age in the dictionary
+
+
--- types.py
+++ types.py
@@ -1,21 +1,85 @@
-print 26
-print "26"
-#number can only stored value as for strings contains any combination of characthers
-print 26 * 30
-print "26" * 30 
-print 76
-print "Addilah"
-#print Addilah #unable to print because Addilah is not define)
-print "My name is Addilah and my age is 26"
-print "26 * 35272356383"
-print "Addilah"+"26"*35272356383
-print 3 == 6 #a
-print 3 <= 6 #b
-print 6 == 6 #c
-print 6 > 6 #d
+#Types Exercises
 
-print 4 == 4 #a
-print 4 =="4" #b
-print == "four" #c
-print 38*38*38 > 383*383 #
+#print age as a number
+age = 36
+print age 
+
+#print age as string 
+age = "36"
+print age
+
+#Difference between age as number ans string?
+#Answer: number can only stored value as for strings contains value and any combination of characthers
+
+#age (as a number) * 30
+age = 36
+print age * 30
+
+#age (as a string) * 30. what happened & why?
+age = "36"
+print age * 30 
+#Answer: 36 was printed 30 times and thats what happen if you * string and number
+
+#print number greater than your age
+age = 36 
+print age + 10 
+
+#print your name
+name = "Addy"
+print name
+
+#print name as quotation marks. wwhat happened & why?
+#name = Addy
+#print name
+#Answer : Error as Addy is not define as a string
+
+#print your name and age on the same line
+details = "my name is Addy and my age is 36"
+print details
+
+#print your age * by 35272356383 as string
+age = "36"
+print int(age) * 35272356383 
+
+#print name & age * 35272356383 on the same line
+age = "36" 
+name = "addy"
+print name + " is not " + str (int (age) * 35272356383)
+
+#result of this code & what is the code doing?
+#Answer: Code is tetsted for truth value as Boolean operation
+
+3 == 6 
+#Answer: False as 3 do NOT equal as 6
+
+3 <= 6 
+#Answer: True as 3 is less than equal to 6
+
+6 == 6 
+#Answer: True as 6 equal to 6
+
+6 > 6 
+#Answer: False as 6 do NOT greater than 6
+
+
+#result of this following code and why?
+
+4 == 4
+#answer: True as 4 equal to 4
+
+4 == "4"
+#answer: False as both side are object as number 4 is no the same "4" (string)
+
+ 4 == "four" 
+ #answer: False as both side are object as number 4 is no the same "four" (string)
+
+#use code to determined if 38*38*38 is greater than 383*383
+print 38*38*38 > 383*383
+#Answer: False as 38*38*38 is NOT greater than 383*383
+
+
+
+
+
+
 

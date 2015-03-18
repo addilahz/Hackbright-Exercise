decc3cc08d833ba897720430076ca604ac7b6bca
Addilah Zakaria <addilahz@gmail.com>
2015-03-18 08:34:35 -0700

    refactor into functions

--- choose_your_adventure.py
+++ choose_your_adventure.py
@@ -1,4 +1,26 @@
 #choose your adventure
+
+def intro ():
+	#print introductions - going in a castle
+	print "Going inside the castle to find treasure. You stand in front of two doors. Please choose and proceeds at your own risk"
+	#decide - choose a door A B
+	print "Door A, Door B or Door C" 
+	#users need to enter A or B in the terminal
+	door = raw_input()
+	door = door.upper()
+	# #whichever door the users choose, it will open
+	if door == "NO":
+	   print "Toodles!"
+	if door == "A" :
+		doorA ()
+	if door == "B":
+		doorB()
+	if door == "C":
+ 		doorC()
+
+
+ 
+
 def doorA ():
 	print "Hooray! Your found the treasure!"
 def doorB ():
@@ -7,39 +29,21 @@ def doorC():
 	print "Congratulations, you found a secret door! You get Easter Eggs as a treat. Enjoy!"
 	print "would you like to continue to play and choose Door A, Door B or No?"
 	door = raw_input()
-	if door == "No":
+	door = door.upper()
+	if door == "NO":
 	   print "Toodles!"
 	if door == "A":
 		doorA()
 	if door == "B":
 		doorB()
+	if door == "C":
+ 		doorC()
+intro()
+ 
 
 
 
 
-
-
-
-
-
-
-
-
-
-#print introductions - going in a castle
-print "Going inside the castle to find treasure. You stand in front of two doors. Please choose and proceeds at your own risk"
-#decide - choose a door A B
-print "Door A or Door B" 
-#users need to enter A or B in the terminal
-door = raw_input()
-#whichever door the users choose, it will open
-if door == "A" :
-	doorA ()
-if door == "B":
-	doorB()
-if door == "C":
-	doorC()
-#door B - find a dragon. users burned! game over 
 
 
 
